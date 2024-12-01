ea0cfe08bedad7d6fecf65edf3d7b598d3c06d34
Joselu <joseluissgz97@gmail.com>
2024-12-01 14:20:39 +0100

    feat: add formatter for date

---
+++ public/utils/formatter.ts
@@ -0,0 +1,9 @@
+export const formatter = (value: Date): string => {
+  const date = new Date(value);
+
+  return Intl.DateTimeFormat('es-Es', {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit'
+  }).format(date);
+};
